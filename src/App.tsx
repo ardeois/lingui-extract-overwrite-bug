91bdd6886bd45bf304c475c021ef4f2321a700c4
Corentin Ardeois <cardeois@landr.com>
2021-11-05 15:42:52 -0400

    chore: update documentation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {t, Trans} from '@lingui/macro';
 import {i18n} from '@lingui/core';
 import {I18nProvider} from '@lingui/react'
 import {messages} from './locales/fr/messages'
-import logo from './logo.svg';
 import './App.css';
 
 i18n.load('en', messages)
@@ -17,17 +16,16 @@ function App() {
     });
     return (
         <I18nProvider i18n={i18n}>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p><Trans>Translation test A</Trans></p>
-                    {/* Have to use this syntax sometimes when we have a single quote, eslint could enforce this syntax */}
-                    <p><Trans>{`Translation test B`}</Trans></p>
-                    <p><Trans id="translation_c">Translation test C</Trans></p>
-                    <p>{translationD}</p>
-                    <p>{translationE}</p>
-                </header>
-            </div>
+            {/* Not working ❌ */}
+            <p><Trans>Translation test A</Trans></p>
+            {/* Not working ❌ */}
+            <p><Trans>{`Translation test B`}</Trans></p>
+            {/* Working ✅ */}
+            <p><Trans id="translation_c">Translation test C</Trans></p>
+            {/* Not working ❌ */}
+            <p>{translationD}</p>
+            {/* Working ✅ */}
+            <p>{translationE}</p>
         </I18nProvider>
     );
 }
