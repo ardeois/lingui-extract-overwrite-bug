72e84b7129761cea988a639d8564a416d8087309
Corentin Ardeois <cardeois@landr.com>
2021-11-05 15:26:53 -0400

    fix: setup lingui project to reproduce issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
+import {t, Trans} from '@lingui/macro';
+import {i18n} from '@lingui/core';
+import {I18nProvider} from '@lingui/react'
+import {messages} from './locales/fr/messages'
 import logo from './logo.svg';
 import './App.css';
 
+i18n.load('en', messages)
+i18n.activate('en')
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const translationD = t`Translation test D`;
+    const translationE = t({
+        id: 'translation_e',
+        message: 'Translation test E'
+    });
+    return (
+        <I18nProvider i18n={i18n}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p><Trans>Translation test A</Trans></p>
+                    {/* Have to use this syntax sometimes when we have a single quote, eslint could enforce this syntax */}
+                    <p><Trans>{`Translation test B`}</Trans></p>
+                    <p><Trans id="translation_c">Translation test C</Trans></p>
+                    <p>{translationD}</p>
+                    <p>{translationE}</p>
+                </header>
+            </div>
+        </I18nProvider>
+    );
 }
 
 export default App;
